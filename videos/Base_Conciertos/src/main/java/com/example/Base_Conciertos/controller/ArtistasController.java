36cde3c13dc13979b22350698b8f3cb40b353cc3
frxnciss <festela@pucp.edu.pe>
2023-11-20 20:54:07 -0500

    video

--- videos/Base_Conciertos/src/main/java/com/example/Base_Conciertos/controller/ArtistasController.java
+++
@@ -1,5 +0,0 @@
-package com.example.Base_Conciertos.controller;
-
-public class ArtistasController {
-
-}
--- videos/Base_Conciertos/src/main/java/com/example/Base_Conciertos/repository/IntegranteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Base_Conciertos.repository;
-
-import com.example.Base_Conciertos.entity.Integrante;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IntegranteRepository extends JpaRepository<Integrante, Integer> {
-}
